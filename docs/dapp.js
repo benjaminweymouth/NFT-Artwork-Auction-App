b30132f55c0428525a618cfd88af2a039dd70b29
benjamin weymouth <bweymout@my.centennialcollege.ca>
2022-02-17 21:46:55 -0500

    change after sunburst

--- docs/dapp.js
+++ docs/dapp.js
@@ -1,6 +1,6 @@
 // @TODO: Update this address to match your deployed ArtworkMarket contract!
 // const contractAddress = "0x7a377fAd8c7dB341e662c93A79d0B0319DD3DaE8";
-const contractAddress = "0x21b2C24e00Dd9A3942109ba45f316dc14cD25a49";
+const contractAddress = "0xd70EbB881801bD17E2009DbcF3BBBCae5033b386";
 
 
 const dApp = {
